e2a4d09f814ef073460400a5363a7a71bf9fd74b
baran.wang <baran.wang@bytedance.com>
2023-07-30 16:19:34 +0800

    feat: init

---
+++ scripts/icon.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('jsonc-parser');
+
+const iconPath = path.join(__dirname, '../icons');
+
+const icons = {
+  light: {},
+  dark: {},
+};
+
+const iconFiles = fs.readdirSync(iconPath);
+
+iconFiles.forEach((file) => {
+  const name = path.basename(file, path.extname(file));
+  if (name.endsWith('_dark')) {
+    icons.dark[name.replace('_dark', '')] = file;
+  } else {
+    icons.light[name] = file;
+    if (!icons.dark[name]) {
+      icons.dark[name] = file;
+    }
+  }
+});
+
+function defineIcons(type) {
+  return Object.entries(icons[type]).reduce((acc, [key, value]) => {
+    acc[key] = {
+      iconPath: `../icons/${value}`,
+    };
+    return acc;
+  }, {});
+}
+
+const darkIconsConfigPath = path.join(__dirname, '../themes/jetbrains-dark-icons.json');
+const lightIconsConfigPath = path.join(__dirname, '../themes/jetbrains-light-icons.json');
+
+const darkIconsConfig = parse(fs.readFileSync(darkIconsConfigPath).toString());
+const lightIconsConfig = { ...darkIconsConfig };
+
+darkIconsConfig.iconDefinitions = defineIcons('dark');
+lightIconsConfig.iconDefinitions = defineIcons('light');
+
+fs.writeFileSync(darkIconsConfigPath, JSON.stringify(darkIconsConfig, null, 2));
+fs.writeFileSync(lightIconsConfigPath, JSON.stringify(lightIconsConfig, null, 2));
